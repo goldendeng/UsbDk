4949df612aadb864a09964a7b0c0d7642f44a2ac
hudeng <907246009@qq.com>
2016-09-07 10:33:23 +0800

    modified buildmsi.bat

---
+++ Tools/Installer/buildmsi_hu.bat
@@ -0,0 +1,50 @@
+SETLOCAL EnableExtensions EnableDelayedExpansion
+
+IF [%UsbDkVersion%] == [] SET UsbDkVersion=99.99.90
+IF [%UsbDkVersion%] == [".."] SET UsbDkVersion=99.99.90
+
+pushd ..\..\Install_Debug\x86
+
+del *.msi *.wixobj *.wixpdb
+
+"C:\Program Files\WiX Toolset v3.8\bin\candle.exe" ..\..\Tools\Installer\UsbDkInstaller.wxs -out UsbDk_Debug.wixobj -dUsbDkVersion=%UsbDkVersion% -dConfig=Debug
+if !ERRORLEVEL! NEQ 0 exit /B 1
+"C:\Program Files\WiX Toolset v3.8\bin\light.exe" UsbDk_Debug.wixobj -out UsbDk_Debug_%UsbDkVersion%_x86.msi -sw1076
+if !ERRORLEVEL! NEQ 0 exit /B 1
+
+popd
+
+pushd ..\..\Install_Debug\x64
+
+del *.msi *.wixobj *.wixpdb
+
+"C:\Program Files\WiX Toolset v3.8\bin\candle.exe" ..\..\Tools\Installer\UsbDkInstaller.wxs -out UsbDk_Debug.wixobj -dUsbDkVersion=%UsbDkVersion% -dConfig=Debug -dUsbDk64Bit=1
+if !ERRORLEVEL! NEQ 0 exit /B 1
+"C:\Program Files\WiX Toolset v3.8\bin\light.exe" UsbDk_Debug.wixobj -out UsbDk_Debug_%UsbDkVersion%_x64.msi -sw1076
+if !ERRORLEVEL! NEQ 0 exit /B 1
+
+popd
+
+pushd ..\..\Install\x86
+
+del *.msi *.wixobj *.wixpdb
+
+"C:\Program Files\WiX Toolset v3.8\bin\candle.exe" ..\..\Tools\Installer\UsbDkInstaller.wxs -out UsbDk.wixobj -dUsbDkVersion=%UsbDkVersion% -dConfig=Release
+if !ERRORLEVEL! NEQ 0 exit /B 1
+"C:\Program Files\WiX Toolset v3.8\bin\light.exe" UsbDk.wixobj -out UsbDk_%UsbDkVersion%_x86.msi -sw1076
+if !ERRORLEVEL! NEQ 0 exit /B 1
+
+popd
+
+pushd ..\..\Install\x64
+
+del *.msi *.wixobj *.wixpdb
+
+"C:\Program Files\WiX Toolset v3.8\bin\candle.exe" ..\..\Tools\Installer\UsbDkInstaller.wxs -out UsbDk.wixobj -dUsbDkVersion=%UsbDkVersion% -dConfig=Release -dUsbDk64Bit=1
+if !ERRORLEVEL! NEQ 0 exit /B 1
+"C:\Program Files\WiX Toolset v3.8\bin\light.exe" UsbDk.wixobj -out UsbDk_%UsbDkVersion%_x64.msi -sw1076
+if !ERRORLEVEL! NEQ 0 exit /B 1
+
+popd
+
+echo SUCCEEDED
